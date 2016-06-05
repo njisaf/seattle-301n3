d9ade03e12678e8c243c1a68dbbb7439765cd060
Patrick Colgan <patrickjcolgan@hotmail.com>
2016-06-05 14:10:09 -0700

    else show all

--- class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/starter-code/scripts/articleView.js
@@ -32,6 +32,7 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+      //DONE
       var $choice = $(this).val();
       $('article').hide();
       $('article[data-author="'+ $choice +'"]').fadeIn(1000);
@@ -40,6 +41,7 @@ articleView.handleAuthorFilter = function() {
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
+      $('article').fadeIn(1000);
 
     }
     $('#category-filter').val('');
